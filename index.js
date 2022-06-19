b6f9066c6842dd9f75ec4d8dbdd44868dea17240
Elena Markova <webkwondo.com@gmail.com>
2022-06-19 21:55:47 +0300

    feat: add app configuration

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import 'dotenv/config';
+import { getDirName, join } from './src/utils.js';
 
 const PORT = process.env.PORT;
+
+const moduleUrl = import.meta.url;
+const __dirname = getDirName(moduleUrl);
+
+const HOST = '127.0.0.1';
+const PORT = process.env.PORT;
+const CONTENT_TYPE = 'application/json';
+
+const apiBaseUrl = '/api/users';
+const storageFilePath = join(__dirname, 'src', 'storage.json');
+
