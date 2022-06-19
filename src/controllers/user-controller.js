d3d184f45057f77871cd78ee723631db9cbada83
Elena Markova <webkwondo.com@gmail.com>
2022-06-19 22:00:46 +0300

    feat: add user controller

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,88 @@
+import { validate as uuidValidate } from 'uuid';
+import { getAllUsers, findUser, addUser, deleteUser, updateUser } from './../models/user-model.js';
+
+const handleAllUsersRequest = async (res) => {
+  res.writeHead(200);
+  res.end(JSON.stringify(getAllUsers()));
+};
+
+const handleUserRequest = async (method, req, res, userId) => {
+
+  const handleUser = async (uid, successCode = 200, action) => {
+
+    if (!uuidValidate(uid)) {
+      res.writeHead(400);
+      res.end(JSON.stringify([{message: `The user id ${uid} is invalid.`}]));
+      return false;
+    }
+
+    let user = findUser(uid);
+
+    if (!user) {
+      res.writeHead(404);
+      res.end(JSON.stringify([{message: `The user with id ${uid} doesn't exist.`}]));
+      return false;
+    }
+
+    try {
+      if (action) {
+        user = [ await action(uid) ];
+      }
+
+      res.writeHead(successCode);
+    } catch (error) {
+      throw error;
+    }
+
+    res.end(JSON.stringify(user));
+    return true;
+  };
+
+  const getReqBody = async () => {
+    const buffer = [];
+
+    for await (const chunk of req) {
+      buffer.push(chunk);
+    }
+
+    const reqBody = Buffer.concat(buffer).toString();
+    const parsedReqBody = JSON.parse(reqBody);
+
+    return parsedReqBody;
+  };
+
+  let reqBodyData = '';
+
+  switch (method) {
+    case 'GET':
+      await handleUser(userId);
+      break;
+    case 'POST':
+      reqBodyData = await getReqBody();
+      const result = await addUser(reqBodyData);
+      const statusCode = (result.is) ? 201 : 400;
+      const msg = (result.record) ? [{message: result.message, record: result.record}]
+                                  : [{message: result.message}];
+
+      res.writeHead(statusCode);
+      res.end(JSON.stringify(msg));
+      break;
+    case 'PUT':
+      reqBodyData = await getReqBody();
+
+      await handleUser(userId, 200, async (uid) => {
+        return await updateUser(uid, reqBodyData);
+      });
+      break;
+    case 'DELETE':
+      await handleUser(userId, 204, async (uid) => {
+        return await deleteUser(uid);
+      });
+      break;
+    default:
+      break;
+  }
+
+};
+
+export { handleAllUsersRequest, handleUserRequest };
